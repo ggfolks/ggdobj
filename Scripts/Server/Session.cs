7993aca28cde8a9027800f040654004289f2d2a8
Ray J. Greenwell <ray@bogocorp.com>
2020-04-02 11:38:32 -0700

    These were changed but missed the last commit.

--- Scripts/Server/Session.cs
+++ Scripts/Server/Session.cs
@@ -121,7 +121,7 @@ public class Session<TRoot> : WebSocketBehavior, ISession where TRoot : Abstract
     }
   }
 
-  private readonly Mutable<string> _userId = Mutable<string>.Local<string>(null);
+  private readonly Mutable<string> _userId = Values.Local<string>(null);
   private Dictionary<uint, DObject> _objectsById = new Dictionary<uint, DObject>();
   private Dictionary<DObject, uint> _idsByObject = new Dictionary<DObject, uint>();
   private Encoder _encoder = new Encoder();
