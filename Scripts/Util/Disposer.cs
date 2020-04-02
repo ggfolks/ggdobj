f2e9bf0e6499bc33526abb456fe1ee29a3fa201d
Ray J. Greenwell <ray@bogocorp.com>
2020-04-02 10:30:23 -0700

    This needs to accept Remover.
    I just added it as Add() but then that makes nearly all existing calls
    ambiguous. Kept the Action overload but renamed.

--- Scripts/Util/Disposer.cs
+++ Scripts/Util/Disposer.cs
@@ -3,25 +3,34 @@
 using System;
 using System.Collections.Generic;
 
+using React;
+
 /// <summary>
 /// Maintains a list of actions to take on disposal.
 /// </summary>
 public class Disposer : IDisposable {
 
   /// <summary>
+  /// Adds a Remover to run on disposal.
+  /// </summary>
+  public void Add (Remover remover) {
+    _removers.Add(remover);
+  }
+
+  /// <summary>
   /// Adds an action to perform on disposal.
   /// </summary>
-  public void Add (Action action) {
-    _actions.Add(action);
+  public void AddAction (Action action) {
+    _removers.Add(() => action());
   }
 
   // defined by IDisposable
   public void Dispose () {
-    foreach (var action in _actions) action();
-    _actions.Clear();
+    foreach (var remover in _removers) remover();
+    _removers.Clear();
   }
 
-  private List<Action> _actions = new List<Action>();
+  private List<Remover> _removers = new List<Remover>();
 }
 
 }
